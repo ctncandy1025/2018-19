1969f9d2cc0f1c9228ea837dc858deda1b396bca
Rohan Ghotra <38005466+Rohan75@users.noreply.github.com>
2018-11-24 11:05:48 -0500

    Create TeleOp.java

---
+++ TeleOp.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="TeleOp2", group="TeleOp")
+public class TeleOp extends OpMode {
+
+    DcMotor FL, FR, BL, BR;
+
+    @Override
+    public void init() {
+        FL = hardwareMap.get(DcMotor.class, "frontLeft");
+        FR = hardwareMap.get(DcMotor.class, "frontRight");
+        BL = hardwareMap.get(DcMotor.class, "backLeft");
+        BR = hardwareMap.get(DcMotor.class, "backRight");
+
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
+
+        double RT = gamepad1.right_trigger;
+        double LT = gamepad1.left_trigger;
+        double LY = -gamepad1.left_stick_y;
+        double RY = -gamepad1.right_stick_y;
+
+        FL.setPower(LY + RT - LT);
+        BL.setPower(LY - RT + LT);
+        FR.setPower(RY + LT - RT);
+        BR.setPower(RY - LT + RT);
+
+    }
+
+}
